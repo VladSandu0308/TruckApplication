4b068786f9ab3f4d0540109a5f2a31910869582a
Mircia Camelia <gabrielacamelia1@gmail.com>
2022-01-11 00:21:02 +0200

    route to login

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import MapMenu from './MapMenu';
+import Login from './Login'
 
 
 
@@ -23,6 +24,7 @@ function App() {
     <Routes>
       <Route exact path="/" element = {<Home/>}/>
       <Route path="/map" element = {<MapMenu/>}/>
+      <Route path="/login" element = {<Login/>}/>
 
     </Routes>
   </div>
