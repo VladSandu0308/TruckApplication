9d9c2e62878140f20f1ac5a2c262bba598785caf
Vlad <vlad.sandu99@yahoo.com>
2022-01-23 16:25:26 +0200

    Debug hopefully

--- src/components/ReviewOrder.js
+++ src/components/ReviewOrder.js
@@ -109,8 +109,8 @@ const Logout = () => {
         price: depPlaceIntPlaceDistance * state.empty_price + intPlaceArivalPlaceDistance * state.full_price,
         pay_deadline: state.pay_deadline.split("T")[0],
         t_id: state.id,
-        dep_date: state.dep_date,
-        arival_date: state.arival_date
+        dep_date: state.dep_date.split("T")[0],
+        arival_date: state.arival_date.split("T")[0]
       });
     } else {
       retBody = await postRequest({
