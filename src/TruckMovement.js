73421839433c36ce250bdd35f677ab594eeb56e5
Andrei <dumitru.andrei1505@gmail.com>
2022-01-24 19:48:08 +0200

    Route remove on arival

--- src/TruckMovement.js
+++ src/TruckMovement.js
@@ -6,6 +6,7 @@ const TruckMovement = (props) => {
     const [lastTruckPointGraphic, setLastTruckPointGraphic] = useState(null);
     const [truckPointIndex, setTruckPointIndex] = useState(0);
     const [timer, setTimer] = useState(new Date());
+    const [finished, setFinished] = useState(0);
 
     useEffect(() => {
         loadModules(["esri/Graphic"]).then(([Graphic]) => {
@@ -21,6 +22,17 @@ const TruckMovement = (props) => {
                     size: "8px"
                 };
 
+                const popupTemplate = {
+                    title: "{Name}",
+                    content: "{Description}"
+                 }
+                 const attributes = {
+                    Name: "Camion " + String(props.t_id),
+                    Description:   "De la: " + String(props.dep_place) + "\n" + "La: " + String(props.arival_place) + "\n" +
+                     "Data plecare: " + String(props.dep_date) + "\n" + "Data sosire: " + String(props.arival_date)
+    
+                 }
+
                 const truckPoint = {
                     type: "point",
                     longitude: currentTruckPoint[0],
@@ -29,7 +41,9 @@ const TruckMovement = (props) => {
 
                 const truckPointGraphic = new Graphic({
                     geometry: truckPoint,
-                    symbol: fillSymbol
+                    symbol: fillSymbol,
+                    attributes: attributes,
+                    popupTemplate: popupTemplate
                 });
 
                 props.view.graphics.add(truckPointGraphic);
@@ -37,11 +51,22 @@ const TruckMovement = (props) => {
             }
 
             setCurrentTruckPoint(props.path[truckPointIndex]);
-            if(truckPointIndex + 25 > props.path.length){
+            if(truckPointIndex + 100 > props.path.length){
                 setTruckPointIndex(props.path.length - 1);
+                props.view.graphics.remove(props.depIntRouteGraphic);
+                props.view.graphics.remove(props.intArivalRouteGraphic);
+                props.view.graphics.remove(lastTruckPointGraphic);
+                fetch('http://localhost:8080/finishContract', {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({id: props.c_id})
+                })
+                setFinished(1);
             }
             else{
-                setTruckPointIndex(truckPointIndex + 25);
+                setTruckPointIndex(truckPointIndex + 100);
             }
 
         })
@@ -50,8 +75,10 @@ const TruckMovement = (props) => {
     useEffect(() => {
         const interval = setInterval(() => {
            console.log('This will run every second!');
-           setTimer(new Date());
-        }, 1000);
+           if(!finished){
+            setTimer(new Date());
+           }
+        }, 5000);
      
         // This is important, you must clear your interval when component unmounts
         return () => clearInterval(interval);
--- src/components/TransporterRequests.js
+++ src/components/TransporterRequests.js
@@ -21,9 +21,9 @@ class TransporterRequests extends React.Component{
             .then((res) => res.json())
             .then((json) => {
                 this.setState({
-                    items: json,
+                    items: json.sort((a, b) => { return a.full_price - b.full_price || a.empty_price - b.empty_price}),
                     DataisLoaded: true
-            });
+            })
         })
     }
 
@@ -44,52 +44,112 @@ class TransporterRequests extends React.Component{
         console.log("Name:" + this.props.name);
    
         return (
-            <div className="card mx-auto" style ={{backgroundColor: '#c4d6b0', padding: '15px', width: '35rem', marginTop: '3rem'}}>
-            <h5 class="card-title" style ={{marginBottom:'20px'}}> Transporters' Offers </h5>
-            <div className = "accordion accordion-flush" id = "accordionFlushExample">
-                  {
-                    items.map((item) => ( 
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingOne">
-                            <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target={format('#{0}', item.t_id)} aria-expanded="false" aria-controls="flush-collapseOne">
-                              {item.username }: {item.dep_place} - {item.arival_place} 
-                            </button>
-                            </h2>
-                            <div id={item.t_id} class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
-                            <div class="accordion-body">
-                               <p> Transportator: {item.username} </p>
-                               <p> Email: {item.email} </p>
-                               <p> Telefon: {item.phone} </p>
-                               <p> Data plecarii: {item.dep_date} </p>
-                               <p> Locul plecarii: {item.dep_place} </p>
-                               <p> Data sosirii:  {item.arival_date} </p>
-                               <p> Destinatie:  {item.arival_place} </p> 
-                               <p> Tip camion: {item.truck_type} </p>
-                               <p> Volum (m3): {item.volume} </p>
-                               <p> Greutate (tone): {item.weight} </p>
-                               <p> Lungime: {item.length} </p>
-                               <p> Latime: {item.width} </p>
-                               <p> Inaltime: {item.height} </p>
-                               <p> Pret/km preluare: {item.empty_price} </p>
-                               <p> Pret/km livrare: {item.full_price} </p>
-                               <Link to={format('/review/{0}', item.t_id)} state={{ 
-                                    client: this.props.name, 
-                                    transporter: item.username,
-                                    dep_place: item.dep_place,
-                                    arival_place: item.arival_place,
-                                    pay_deadline: item.arival_date,
-                                    myself: "Client",
-                                    id: item.t_id,
-                                    empty_price: item.empty_price,
-                                    full_price: item.full_price
-                                }} className="btn btn-info font-weight-bold">Accept Offer</Link>
+            <div>
+                <div className="card mx-auto" style ={{backgroundColor: '#c4d6b0', padding: '15px', width: '35rem', marginTop: '3rem'}}>
+                    <h5 class="card-title" style ={{marginBottom:'20px'}}> Transporters' Recommendations </h5>
+                    <div className = "accordion accordion-flush" id = "accordionFlushExample">
+                    {
+                            items.slice(0,2).map((item) => ( 
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="flush-headingOne">
+                                    <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target={format('#{0}', item.t_id)} aria-expanded="false" aria-controls="flush-collapseOne">
+                                    {item.username }: {item.dep_place} - {item.arival_place} 
+                                    </button>
+                                    </h2>
+                                    <div id={item.t_id} class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                    <p> Transportator: {item.username} </p>
+                                    <p> Email: {item.email} </p>
+                                    <p> Telefon: {item.phone} </p>
+                                    <p> Data plecarii: {item.dep_date} </p>
+                                    <p> Locul plecarii: {item.dep_place} </p>
+                                    <p> Data sosirii:  {item.arival_date} </p>
+                                    <p> Destinatie:  {item.arival_place} </p> 
+                                    <p> Tip camion: {item.truck_type} </p>
+                                    <p> Volum (m3): {item.volume} </p>
+                                    <p> Greutate (tone): {item.weight} </p>
+                                    <p> Lungime: {item.length} </p>
+                                    <p> Latime: {item.width} </p>
+                                    <p> Inaltime: {item.height} </p>
+                                    <p> Pret/km preluare: {item.empty_price} </p>
+                                    <p> Pret/km livrare: {item.full_price} </p>
+                                    <Link to={format('/review/{0}', item.t_id)} state={{ 
+                                            client: this.props.name, 
+                                            transporter: item.username,
+                                            dep_place: item.dep_place,
+                                            arival_place: item.arival_place,
+                                            pay_deadline: item.arival_date,
+                                            myself: "Client",
+                                            id: item.t_id,
+                                            empty_price: item.empty_price,
+                                            full_price: item.full_price,
+                                            dep_date: item.dep_date,
+                                            arival_date: item.arival_date
+                                        }} className="btn btn-info font-weight-bold">Accept Offer</Link>
+                                    </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    </div>
+                {
+                    items.length > 2 ? (
+                        <div className="card mx-auto" style ={{backgroundColor: '#c4d6b0', padding: '15px', width: '35rem', marginTop: '3rem'}}>
+                            <h5 class="card-title" style ={{marginBottom:'20px'}}> Transporters' Offers </h5>
+                            <div className = "accordion accordion-flush" id = "accordionFlushExample">
+                                {
+                                    items.slice(2).map((item) => ( 
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="flush-headingOne">
+                                            <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target={format('#{0}', item.t_id)} aria-expanded="false" aria-controls="flush-collapseOne">
+                                            {item.username }: {item.dep_place} - {item.arival_place} 
+                                            </button>
+                                            </h2>
+                                            <div id={item.t_id} class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
+                                            <div class="accordion-body">
+                                            <p> Transportator: {item.username} </p>
+                                            <p> Email: {item.email} </p>
+                                            <p> Telefon: {item.phone} </p>
+                                            <p> Data plecarii: {item.dep_date} </p>
+                                            <p> Locul plecarii: {item.dep_place} </p>
+                                            <p> Data sosirii:  {item.arival_date} </p>
+                                            <p> Destinatie:  {item.arival_place} </p> 
+                                            <p> Tip camion: {item.truck_type} </p>
+                                            <p> Volum (m3): {item.volume} </p>
+                                            <p> Greutate (tone): {item.weight} </p>
+                                            <p> Lungime: {item.length} </p>
+                                            <p> Latime: {item.width} </p>
+                                            <p> Inaltime: {item.height} </p>
+                                            <p> Pret/km preluare: {item.empty_price} </p>
+                                            <p> Pret/km livrare: {item.full_price} </p>
+                                            <Link to={format('/review/{0}', item.t_id)} state={{ 
+                                                    client: this.props.name, 
+                                                    transporter: item.username,
+                                                    dep_place: item.dep_place,
+                                                    arival_place: item.arival_place,
+                                                    pay_deadline: item.arival_date,
+                                                    myself: "Client",
+                                                    id: item.t_id,
+                                                    empty_price: item.empty_price,
+                                                    full_price: item.full_price,
+                                                    dep_date: item.dep_date,
+                                                    arival_date: item.arival_date
+                                                }} className="btn btn-info font-weight-bold">Accept Offer</Link>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
                             </div>
                             </div>
-                        </div>
-                    ))
+                    ) : (
+                        <></>
+                    )
                 }
+                
             </div>
-            </div>
+            
         );
     }
 
